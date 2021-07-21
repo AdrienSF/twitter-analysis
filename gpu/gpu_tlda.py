713afe7f83b48a01cb9b37c3c32041dec167009d
Adrien Schurger-Foy <adriensf@gmail.com>
2021-07-21 12:01:07 -0700

    try gpu backend tlda

---
+++ gpu/gpu_tlda.py
@@ -0,0 +1,126 @@
+# import nvcategory
+# import nvstrings
+from datetime import datetime
+import cudf
+import cuml
+# import numpy as np
+import cupyx
+from cuml.feature_extraction.text import HashingVectorizer
+from cuml.feature_extraction.text import CountVectorizer
+# from cuml import PCA
+# from cuml.decomposition import PCA
+from cuml.decomposition import IncrementalPCA
+import cupy as cp
+from pca_cupy import PCA
+import tensorly as tl
+tl.set_backend('cupy')
+
+
+
+# load tweets
+import os
+import pickle
+import pandas as pd
+from helpers import load_tweets
+# all_tweets = load_tweets(filenames, preprocessor=None)
+
+
+print('loading pickle')
+filename = '../clean_data/2020-04-22_23-55-53--2020-04-29_23-55-53.pickle'
+with open(filename, 'rb') as f:
+    df = pd.DataFrame(pickle.load(f), columns =['date', 'tweet'])
+
+
+
+
+print("df['tweet']", df['tweet'].shape)
+print('subsanmpling...')
+import random
+# use only 100000 tweets
+all_tweets = list(df['tweet'].values)
+tweets = random.sample(all_tweets, 100000)
+
+
+tweets = cudf.Series(tweets)
+print("tweets shape", tweets.shape)
+n_samples = len(tweets)
+
+
+vec = CountVectorizer(stop_words='english',
+                     lowercase = True, # works
+                    ngram_range = (1,2),
+                    max_df = 0.4, # works
+                    min_df = int(0.002*n_samples))
+                    
+print('vectorizing...')
+
+dtm = vec.fit_transform(tweets)
+print('converting to cupy...')
+dtm_sent = cupyx.scipy.sparse.csr_matrix(dtm)
+
+print('converting to tensor...')
+a = tl.tensor(dtm_sent.toarray(),dtype=cp.float16)
+M1 = tl.mean(a, axis=0)
+print('centering...')
+x_cent = cupyx.scipy.sparse.csr_matrix(dtm - M1) #center the data using the first moment 
+
+
+batch_size = 240000
+verbose = True
+n_topic =  20
+
+beta_0=0.003
+
+
+from cuml.decomposition import IncrementalPCA
+start = datetime.now()
+print("now =", start)
+
+
+batch_size = 240000
+verbose = True
+n_topic =  20
+
+beta_0=0.003
+
+pca = IncrementalPCA(n_components = n_topic, batch_size=batch_size, whiten=True)
+print('fitting pca...')
+pca.fit(x_cent) # fits PCA to  data, gives W
+print('pca transforming data...')
+x_whit = pca.transform(x_cent) # produces a whitened words counts <W,x> for centered data x
+now = datetime.now()
+print("now =", now)
+pca_time = now - start 
+
+print("x_whit", x_whit.shape)
+
+
+
+from importlib import reload  
+import tlda_final_cupy
+reload(tlda_final_cupy)
+from tlda_final_cupy import TLDA
+
+
+now = datetime.now()
+print("now =", now)
+learning_rate = 0.01 
+batch_size =240000
+t = TLDA(n_topic,n_senti=1, alpha_0= beta_0, n_iter_train=1000, n_iter_test=150, batch_size=batch_size,
+         learning_rate=learning_rate)
+now = datetime.now()
+print("now =", now)
+
+
+print("t.factors_.shape", t.factors_.shape)
+
+
+
+now = datetime.now()
+print('fitting tlda...')
+print("now =", now)
+t.fit(x_whit,verbose=True) # fit whitened wordcounts to get decomposition of M3 through SGD
+
+now = datetime.now()
+print("now =", now)
+print('DONE!')
