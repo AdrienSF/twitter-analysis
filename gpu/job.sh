b62fc0dac095f2602b8617c3330ae6aa305bbfe9
Adrien Schurger-Foy <adriensf@gmail.com>
2021-07-21 12:18:44 -0700

    try gpu backend tlda

---
+++ gpu/job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=gpuq
+#SBATCH --nodes=1
+#SBATCH --time=0-4:00:00
+#SBATCH --job-name=complexity_measure
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
+
+module load anaconda3/current
+eval "$(conda shell.bash hook)"
+conda init bash
+conda activate rapids-0.19
+
+python3 gpu_tlda.py
+
