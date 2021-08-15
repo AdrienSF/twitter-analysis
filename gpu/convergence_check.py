47f8472bdf6cf27fec9ce5e4b443f0712ef0b6a1
adrien_desktop <adriensf@gmail.com>
2021-08-15 12:22:39 -0700

    complete convergence search

--- gpu/convergence_check.py
+++ gpu/convergence_check.py
@@ -1,13 +1,13 @@
 import pickle, os
 import cupy
 
-def are_similar(run1, run2, top_words, tolerance):
-    similarity = all([any([len(set(list(run1[i].keys())[-top_words:]) - set(list(run2[j].keys())[-top_words:])) <= tolerance for i in range(n_topics)]) for j in range(n_topics)])
+def get_similarity(run1, run2, top_words, tolerance):
+    similarity = sum([any([len(set(list(run1[i].keys())[-top_words:]) - set(list(run2[j].keys())[-top_words:])) <= tolerance for i in range(n_topics)]) for j in range(n_topics)])
     return similarity
 
 
 
-print('loading whitened...')
+#print('loading whitened...')
 filenames  = ['convergence_check/whitened/' + name for name in os.listdir('convergence_check/whitened')]
 whiteneds = []
 for name in filenames:
@@ -15,17 +15,17 @@ for name in filenames:
         whiteneds.append(pickle.load(f))
 
 
-print('comparing whitened...')
+#print('comparing whitened...')
 
 tolerance = 1e-10
 converges = all([all([bool(cupy.allclose(run2, run1, rtol=0, atol=tolerance)) for run1 in whiteneds]) for run2 in whiteneds])
+assert converges
+#print('converges:', converges)
 
-print('converges:', converges)
-
-print()
+#print()
 
 
-print('loading distributions...')
+#print('loading distributions...')
 filenames  = ['convergence_check/distributions/' + name for name in os.listdir('convergence_check/distributions')]
 distributions = []
 for name in filenames:
@@ -37,9 +37,10 @@ print('comparing distributions...')
 n_topics = len(distributions[0])
 
 top_words = 10
-tolerance = 3
+word_tolerance = 5
+similarity_thresh = n_topics/2
 
-converges = all([all([are_similar(run1, run2, top_words, tolerance) for run1 in distributions]) for run2 in distributions])
+convergence = all([all([get_similarity(run1, run2, top_words, word_tolerance) >= similarity_thresh  for run1 in distributions]) for run2 in distributions])
 
-
-print('converges:', converges)
+print('top_words word_tolerance similarity_thresh |', top_words, word_tolerance, similarity_thresh)
+print('converges:', convergence)
