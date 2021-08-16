366483b78493bb42bd0750fa0f5ae49564661bfc
adrien_desktop <adriensf@gmail.com>
2021-08-16 06:16:25 -0700

    complete convergence search

--- gpu/convergence_search.sh
+++ gpu/convergence_search.sh
@@ -1,5 +1,5 @@
-for learning_rate in 0.01 0.005 0.001; do
-    for n_iter_train in 1000 2000 3000; do
+for learning_rate in 0.001 0.0005 0.0001; do
+    for n_iter_train in 1000 2000 3000 4000 5000 6000; do
         echo "python3 mem_comprimize.py $learning_rate $n_iter_train" >> convergence_search.txt
         python3 mem_compromize.py $learning_rate $n_iter_train
         python3 convergence_check.py >> convergence_search.txt
