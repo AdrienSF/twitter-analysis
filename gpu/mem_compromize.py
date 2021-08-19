15f08c8bbb7b9af9b37b2773d9c1273ede74c78e
Adrien Schurger-Foy <adriensf@gmail.com>
2021-08-19 13:10:23 -0700

    .

--- gpu/mem_compromize.py
+++ gpu/mem_compromize.py
@@ -31,8 +31,7 @@ def shuffle_forward(l):
     return list(np.array(l)[order]), order
 
 def shuffle_backward(l, order):
-    l = l.to_numpy()
-    l_out = np.zeros((l.shape))
+    l_out = cp.zeros((l.shape))
     for i, j in enumerate(order):
         l_out[j] = l[i]
     return l_out
